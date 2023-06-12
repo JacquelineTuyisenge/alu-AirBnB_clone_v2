38cec367420346513dee9d672b82ed64c13f1bd3
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-06-12 20:12:27 +0200

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_base_model import TestBaseModel
 from models.user import User
 
 
-class test_User(test_basemodel):
-    """ """
+class TestUser(TestBaseModel):
+    """ Test for user"""
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -16,23 +16,23 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
-        new.first_name = "Jacqueline"
+        new.first_name = "Chyna"
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
-        new.last_name = "Tuyisenge"
+        new.last_name = "Chyna"
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
         new = self.value()
-        new.email = "tuyisengejacqui@gmail.com"
+        new.email = "angoyewally@gmail.com"
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
-        new.password = "12345j"
+        new.password = "123aashja"
         self.assertEqual(type(new.password), str)
