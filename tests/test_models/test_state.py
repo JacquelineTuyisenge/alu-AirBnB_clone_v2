1a8b7280e72d198bbaf551f9977f521f0d7b586c
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-06-12 19:21:38 +0200

    state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -5,15 +5,16 @@ from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """ test for state """
 
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
-        self.name = "State"
+        self.name = "California"
         self.value = State
 
     def test_name3(self):
         """ """
         new = self.value()
+        new.name = "Arizona"
         self.assertEqual(type(new.name), str)
