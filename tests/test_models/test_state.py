6be2d95a7df83fe3f4af3cdd2c3874974fb812f8
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-06-14 18:25:10 +0200

    test state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import TestBasemodel
+from tests.test_models.test_base_model import TestBaseModel
 from models.state import State
 
 
-class TestState(TestBasemodel):
-    """ test for state """
+class TestState(TestBaseModel):
+    """Test for state """
 
     def __init__(self, *args, **kwargs):
         """ """
